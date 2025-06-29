e67114b9786d1336f2d426c377ad4d26fbbe6c15
Mahdiyar Delavari <MahdiyarDelavari@gmail.com>
2025-06-29 16:25:05 +0330

    implement the first part of the hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
           <div>
               <a href="#home" className='flex items-center gap-2'>
                   <img src="/public/images/logo.png" alt="logo" />
-                  <p>Velvet Pour</p>
+                  <p>Attari</p>
               </a>
 
               <ul>
